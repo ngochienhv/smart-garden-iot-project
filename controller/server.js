04dcf87c8ef24118f21467ee4d749bb2eb648f32
HienNguyen <ngochien123hv@gmail.com>
2022-03-26 22:53:04 +0700

    rearrange to mvc model

--- controller/server.js
+++ controller/server.js
@@ -6,6 +6,6 @@ const port = process.env.PORT || 5000; //Line 3
 app.listen(port, () => console.log(`Listening on port ${port}`)); //Line 6
 
 // create a GET route
-app.get('/express_backend', (req, res) => { //Line 9
-    res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
-}); //Line 11
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
